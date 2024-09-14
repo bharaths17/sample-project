5b3574a999fed8a4475069d7809dba90782ff337
Bharath Pakala <103256903+bharaths17@users.noreply.github.com>
2024-09-14 18:11:34 +0530

    Create HelloController.java

---
+++ java-microservices-project/src/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.service1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Service 1!";
+    }
+}
